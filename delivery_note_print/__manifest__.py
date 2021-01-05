4b67439413db91191b5ba8e3a8354681fa7c59aa
Vishnu Ramesh <55851192+VISHNUVR-LOYALITSOLUTIONS@users.noreply.github.com>
2021-01-05 14:54:25 +0530

    del print

---
+++ delivery_note_print/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Delivery Note Print",
+
+    'summary': """
+        Delivery note print""",
+
+    'description': """
+        Delivery note print
+    """,
+
+    'author': "Loyal IT Solutions Pvt LTd",
+    'website': "http://www.loyalitsolutions.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock', 'account', 'web', 'stock_operating_unit'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
