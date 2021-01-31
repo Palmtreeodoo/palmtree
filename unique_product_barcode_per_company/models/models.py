cb6913e8157c329dbec760d1c419bd47c080542e
Vishnu Ramesh <55851192+VISHNUVR-LOYALITSOLUTIONS@users.noreply.github.com>
2021-01-31 16:54:54 +0530

    Add files via upload

--- unique_product_barcode_per_company/models/models.py
+++ unique_product_barcode_per_company/models/models.py
@@ -4,15 +4,11 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
-class ProductProduct(models.Model):
-    _inherit = "product.product"
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
 
-    _sql_constraints = [
-        ('barcode_uniq', 'Check(1=1)', "A barcode can only be assigned to one product !"),
-    ]
-
-    @api.constrains('barcode')
-    def _check_plu(self):
+    @api.constrains('company_id')
+    def _check_barcode(self):
         if self.barcode:
             product_rec = self.env['product.product'].search_count(
                 [('barcode', '=', self.barcode), ('company_id', '=', self.company_id.id)])
@@ -20,7 +16,19 @@ class ProductProduct(models.Model):
                 raise ValidationError(_('A barcode can only be assigned to one product !'))
 
 
-# class ProductTemplate(models.Model):
-#     _inherit = 'product.template'
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    _sql_constraints = [
+        ('barcode_uniq', 'Check(1=1)', "A barcode can only be assigned to one product !"),
+    ]
+
+    @api.constrains('barcode', 'company_id')
+    def _check_barcode(self):
+        if self.barcode:
+            product_rec = self.env['product.product'].search_count(
+                [('barcode', '=', self.barcode), ('company_id', '=', self.company_id.id)])
+            if product_rec > 1:
+                raise ValidationError(_('A barcode can only be assigned to one product !'))
 
 
