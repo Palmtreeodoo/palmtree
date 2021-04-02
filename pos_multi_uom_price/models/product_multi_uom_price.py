b18b4cb5e886ba54f58884fe4f8968c528294ac3
Vishnu Ramesh <55851192+VISHNUVR-LOYALITSOLUTIONS@users.noreply.github.com>
2021-04-02 21:25:35 +0530

    Add files via upload

--- pos_multi_uom_price/models/product_multi_uom_price.py
+++ pos_multi_uom_price/models/product_multi_uom_price.py
@@ -6,13 +6,13 @@ from odoo import models, fields, api, _
 
 class multi_uom(models.Model):
     _name = 'product.multi.uom.price'
-    _description = 'Product multiple uom price'     
+    _description = 'Product multiple uom price'
 
-    product_id = fields.Many2one('product.product', _('Product'), ondelete='set null', readonly=True)
+    product_id = fields.Many2one('product.template', _('Product'), ondelete='set null', readonly=True)
     category_id = fields.Many2one('uom.category', _('Category'), ondelete='set null', readonly=True)
     uom_id = fields.Many2one('uom.uom', string=_("Unit of Measure"), required=True)
     price = fields.Float(_('Price'), required=True, digits=dp.get_precision('Product Price'))
-    
+
     # EHDLF Categoría del producto
     @api.onchange('product_id')
     def categorydefault(self):
@@ -20,7 +20,7 @@ class multi_uom(models.Model):
             self.category_id = False
         else:
             self.category_id = self.product_id.uom_id.category_id.id
-    
+
     # EHDLF UOM's de la categoria
     @api.onchange('product_id')
     def categorysuom(self):
@@ -29,10 +29,10 @@ class multi_uom(models.Model):
         # EHDLF misma categoría y diferente base
         domain = {'uom_id': [('category_id', '=', self.product_id.uom_id.category_id.id),'!',('id', '=', self.product_id.uom_id.id)]}
         return {'domain': domain}
-    
+
     # EHDLF Convinación Producto-UOM debe ser única
     _sql_constraints = [
         ('product_multi_uom_price_uniq',
          'UNIQUE (product_id,uom_id)',
          _('Product-UOM must be unique and there are duplicates!'))]
-    
+
